a5a43ab18098b402d286386c50029add425ffdc3
Yoooum <akyomi@qq.com>
2023-04-18 09:15:21 +0800

    Initial commit

---
+++ src/main/java/com/prprv/customs/FreightCustomsApplication.java
@@ -0,0 +1,13 @@
+package com.prprv.customs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreightCustomsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreightCustomsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prprv/customs/FreightCustomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prprv.customs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreightCustomsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
