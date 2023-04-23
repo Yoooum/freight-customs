4f0a00d2b2d37777da9ee5d9804f35cf45e84f5e
Christine <Ty.christine@outlook.com>
2023-04-23 09:08:51 +0800

    LOGBACK改为LOG4J2

--- src/main/java/com/prprv/customs/FreightCustomsApplication.java
+++ src/main/java/com/prprv/customs/FreightCustomsApplication.java
@@ -1,13 +1,16 @@
 package com.prprv.customs;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class FreightCustomsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FreightCustomsApplication.class, args);
+        log.info("FreightCustomsApplication started");
     }
 
 }
