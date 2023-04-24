0241d415145d23d06942ed7db35cb58bd093b105
Christine <Ty.christine@outlook.com>
2023-04-24 11:29:44 +0800

    add: Annotation on entity.

--- src/main/java/com/prprv/customs/entity/Customer.java
+++ src/main/java/com/prprv/customs/entity/Customer.java
@@ -1,9 +1,20 @@
 package com.prprv.customs.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * @author Yoooum
  */
+@Data
+@AllArgsConstructor
+@TableName("customer")
 public class Customer {
+
+    @TableId(type = IdType.AUTO)
     private Long id;
     private String name;
     private String contact;
