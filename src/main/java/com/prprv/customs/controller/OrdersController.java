c0b0fdb7f5385100f6bfb3572e6bbd6f4b7163b7
2374347832 <2374347832@qq.com>
2023-04-25 09:03:17 +0800

    orders测试提交文件

---
+++ src/main/java/com/prprv/customs/controller/OrdersController.java
@@ -0,0 +1,11 @@
+package com.prprv.customs.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/order")
+public class OrdersController {
+}
